4e2592991c155958578281ac48cc4a68aac49913
Jahirsf21 <deislhersanchezfunez@gmail.com>
2026-05-09 15:31:28 -0600

    fix: formato

--- programa/acceso_datos.py
+++ programa/acceso_datos.py
@@ -1,7 +1,9 @@
+from vistas import mostrar_marcas
+
 def cargar_marcas():
     ruta_archivo = "archivos/aviones.txt"
     try:
-        with open(ruta_archivo, 'r', encoding='utf-8') as archivo:
+        with open(ruta_archivo, "r", encoding="utf-8") as archivo:
             contenido = archivo.readlines()
         return contenido
     except:
@@ -10,7 +12,7 @@ def cargar_marcas():
 def cargar_modelos():
     ruta_archivo = "archivos/modeloAviones.txt"
     try:
-        with open(ruta_archivo, 'r', encoding='utf-8') as archivo:
+        with open(ruta_archivo, "r", encoding="utf-8") as archivo:
             contenido = archivo.readlines()
         return contenido
     except:
@@ -20,8 +22,20 @@ def cargar_modelos():
 def cargar_usuarios():
     ruta_archivo = "archivos/acceso.txt"
     try:
-        with open(ruta_archivo, 'r', encoding='utf-8') as archivo:
+        with open(ruta_archivo, "r", encoding="utf-8") as archivo:
             contenido = archivo.readlines()
         return contenido
     except:
         print("Error: el archivo acceso.txt no existe")
+
+
+def cargar_aviones():
+    ruta_archivo = "archivos/avionesAerolineas.txt"
+    try:
+        with open(ruta_archivo, "r", encoding="utf-8") as archivo:
+            contenido = archivo.readlines()
+        return contenido
+    except:
+        print("Error: el archivo acceso.txt no existe")
+
+
