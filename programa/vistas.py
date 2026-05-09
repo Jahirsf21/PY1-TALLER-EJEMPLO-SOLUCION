b057998fc5172a8fbf7af4a571d49536b1dad05e
Jahirsf21 <deislhersanchezfunez@gmail.com>
2026-05-09 16:02:43 -0600

    add: -mostrar modelos

--- programa/vistas.py
+++ programa/vistas.py
@@ -11,3 +11,18 @@ def mostrar_marcas():
             i+=1
     except:
         print("Error: el archivo aviones.txt no existe")
+
+def mostrar_modelos():
+    ruta_archivo = "archivos/modeloAviones.txt"
+    try:
+        with open(ruta_archivo, 'r', encoding='utf-8') as archivo:
+            contenido = archivo.readlines()
+        print("Modelos disponibles:")
+        i = 1
+        for modelos in contenido:
+            modelo = modelos.strip().split(";")[0]
+            print(f"{i}:{modelo}")
+            i+=1
+    except:
+        print("Error: el archivo modeloAviones.txt no existe")
+
