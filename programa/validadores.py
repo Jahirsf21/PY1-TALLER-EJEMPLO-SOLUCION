bc3f8a3a491dfe8ffb128bcb345fffd9b6d73b23
Jahirsf21 <deislhersanchezfunez@gmail.com>
2026-05-09 15:33:29 -0600

    add/fix: -validacion de existencia de modelo -validacion de modelo asociado a un avion -corrección en las validaciones para eliminar espacios al comparar campos

--- programa/validadores.py
+++ programa/validadores.py
@@ -1,6 +1,6 @@
-from acceso_datos import cargar_marcas, cargar_modelos
+from acceso_datos import cargar_marcas, cargar_modelos, cargar_aviones
 
-def validar_nombre(nombre):
+def validar_nombre_marca(nombre):
     contador_digitos = 0
     contador_letras = 0
     for caracter in nombre:
@@ -10,10 +10,27 @@ def validar_nombre(nombre):
             contador_letras += 1
     if len(nombre) == contador_digitos:
         return False
+    elif len(nombre) == 1:
+        return False
     else:
         return True
-        
+    
+def validar_nombre_modelo(nombre):
+    contador_digitos = 0
+    contador_letras = 0
+    for caracter in nombre:
+        if caracter.isdigit():
+            contador_digitos += 1
+        else:
+            contador_letras += 1
+    if len(nombre) == contador_digitos:
+        return False
+    elif len(nombre) == 1:
+        return False
+    else:
+        return True
 
+        
 def existe_marca(marca_ingresada):
     marcas_cargadas = cargar_marcas()
     if not marcas_cargadas:
@@ -21,7 +38,7 @@ def existe_marca(marca_ingresada):
     else:
         for marcas in marcas_cargadas:
             marca = marcas.strip()
-            if marca_ingresada == marca:
+            if marca_ingresada.strip() == marca:
                 return True
         return False
     
@@ -32,6 +49,30 @@ def existe_marca_asociada(marca_ingresada):
     else:
         for modelos in modelos_cargados:
             marca = modelos.strip().split(";")[1]
-            if marca_ingresada == marca:
+            if marca_ingresada.strip() == marca:
                 return True
         return False
+
+def existe_modelo(modelo_ingresado):
+    modelos_cargados = cargar_modelos()
+    if not modelos_cargados:
+        return False
+    else:
+        for modelos in modelos_cargados:
+            modelo = modelos.strip().split(";")[0]
+            if modelo_ingresado.strip() == modelo:
+                return True
+        return False
+    
+def existe_modelo_asociado(modelo_ingresado):
+    aviones_cargados = cargar_aviones()
+    if not aviones_cargados:
+        return False
+    else:
+        for aviones in aviones_cargados:
+            modelo = aviones.strip().split(";")[2]
+            if modelo_ingresado.strip() == modelo:
+                return True
+        return False
+    
+
