6878e3fdcda84fcd5cd5fbe71d3cb8c4e78ce91a
Jahirsf21 <deislhersanchezfunez@gmail.com>
2026-05-10 02:56:50 -0600

    add/refactor: - mostrar_aerolineas - ajuste en la forma de mostrar modelos y marcas

--- programa/vistas.py
+++ programa/vistas.py
@@ -7,7 +7,7 @@ def mostrar_marcas():
         i = 1
         for marcas in contenido:
             marca = marcas.strip()
-            print(f"{i}:{marca}")
+            print(f"{i}: Nombre marca: {marca}")
             i+=1
     except:
         print("Error: el archivo aviones.txt no existe")
@@ -21,8 +21,27 @@ def mostrar_modelos():
         i = 1
         for modelos in contenido:
             modelo = modelos.strip().split(";")[0]
-            print(f"{i}:{modelo}")
+            marca = modelos.strip().split(";")[1]
+            asientos_ejecutiva = modelos.strip().split(";")[2]
+            asientos_turista = modelos.strip().split(";")[3]
+            asientos_economica = modelos.strip().split(";")[4]
+            print(f"{i}:Nombre modelo: {modelo} Nombre marca: {marca} Asientos clase ejecutiva: {asientos_ejecutiva} Asientos clase turista: {asientos_turista} Asientos clase economica: {asientos_economica}")
             i+=1
     except:
         print("Error: el archivo modeloAviones.txt no existe")
+
+def mostrar_aerolineas():
+    ruta_archivo = "archivos/aerolineas.txt"
+    try:
+        with open(ruta_archivo, 'r', encoding='utf-8') as archivo:
+            contenido = archivo.readlines()
+        print("\nAerolineas disponibles:")
+        i = 1
+        for aerolineas in contenido:
+            aerolinea = aerolineas.strip().split(";")[0]
+            centro_operaciones = aerolineas.strip().split(";")[1]
+            print(f"{i}: Nombre aerolinéa: {aerolinea} Centro de operaciones en: {centro_operaciones}")
+            i+=1
+    except:
+        print("Error: el archivo aerolineas.txt no existe")
 
